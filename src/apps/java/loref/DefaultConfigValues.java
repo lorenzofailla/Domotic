c0b635ffecb725a97006c3015e01cec35a169309
lorenzofailla <lorenzofailla.developer@gmail.com>
2018-10-24 10:13:38 +0200

    -

--- src/apps/java/loref/DefaultConfigValues.java
+++ src/apps/java/loref/DefaultConfigValues.java
@@ -5,7 +5,16 @@ public class DefaultConfigValues {
 	public static final String CONFIG_FILE_LOCATION = "/etc/domotic/domotic.conf";
 	public static final String LOCAL_COMMAND_DIRECTORY = "/var/lib/domotic";
 		
+	public static final String ERROR = "[error]";
+	
 	public final static long CONNECTION_CHECK_INTERVAL_TIMEOUT_LONG=10000L;
 	public final static long CONNECTION_CHECK_INTERVAL_TIMEOUT_SHORT=10000L;
+	
+	public final static long DEVICE_STATUS_UPDATE_RATE=60000L;
+	public final static long DEVICE_NETWORK_STATUS_UPDATE_RATE=3600000L;
+	
+	public final static long FIREBASE_DB_UPDATE_TIMEOUT=10000L;
+	
+	
 	
 }
